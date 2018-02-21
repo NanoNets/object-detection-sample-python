ec7ea99229d28cd1d1238fc3b949dcfc42a79a5a
Sarthak Jain <2438605+sjain07@users.noreply.github.com>
2018-02-22 02:21:59 +0530

    Update upload-training.py

--- code/upload-training.py
+++ code/upload-training.py
@@ -6,7 +6,7 @@ model_id = os.environ.get('NANONETS_MODEL_ID')
 api_key = os.environ.get('NANONETS_API_KEY')
 
 for root, dirs, files in os.walk(pathToAnnotations, topdown=False):
-    for name in files[0:3]:
+    for name in files:
         annotation = open(os.path.join(root, name), "r")
         filePath = os.path.join(root, name)
         imageName, ext = name.split(".")
