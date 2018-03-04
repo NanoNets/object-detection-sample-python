d5247ae25ae81327b1b0f5e82b522ab68d7cafa6
Sarthak Jain <sarthakjain@gmail.com>
2018-03-04 16:56:50 +0530

    removed spaces

--- code/upload-training.py
+++ code/upload-training.py
@@ -13,17 +13,8 @@ for root, dirs, files in os.walk(pathToAnnotations, topdown=False):
         imagePath = os.path.join(pathToImages, imageName + '.jpg')
         jsonData = annotation.read()
         url = 'https://app.nanonets.com/api/v2/ObjectDetection/Model/' + model_id + '/UploadFile/'
-
-        data = {'file' :open(imagePath, 'rb'),  'data' :('', '[{"filename":"' + imageName+".jpg" + '", "object": '+ jsonData+'}]'),   'modelId' :('', model_id)}
-        
+        data = {'file' :open(imagePath, 'rb'),  'data' :('', '[{"filename":"' + imageName+".jpg" + '", "object": '+ jsonData+'}]'),   'modelId' :('', model_id)}       
         response = requests.post(url, auth=requests.auth.HTTPBasicAuth(api_key, ''), files=data)
-
         print(response.text)
-
-
-
-
-
-#data = {'file' :open('REPLACE_IMAGE_PATH.jpg', 'rb'),     'data' :('', '[{"filename":"REPLACE_IMAGE_PATH.jpg", "object": [{"name":"category", "bndbox": {"xmin": 1,"ymin": 1,"xmax": 100, "ymax": 100}}]}]'),    'modelId' :('', 'REPLACE_MODEL_ID')}
 
 print("\n\n\nNEXT RUN: python ./code/train-model.py")
