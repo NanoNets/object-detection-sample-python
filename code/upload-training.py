59e1fed6354e673b3cf2fcec675f2f5f5bc6cd74
Sarthak Jain <sarthakjain@gmail.com>
2018-08-13 18:04:16 +0530

    no message

--- code/upload-training.py
+++ code/upload-training.py
@@ -6,8 +6,8 @@ pathToImages = './images'
 model_id = os.environ.get('NANONETS_MODEL_ID')
 api_key = os.environ.get('NANONETS_API_KEY')
 
-for root, dirs, files in tqdm(os.walk(pathToAnnotations, topdown=False)):
-    for name in files:
+for root, dirs, files in os.walk(pathToAnnotations, topdown=False):
+    for name in tqdm(files):
         annotation = open(os.path.join(root, name), "r")
         filePath = os.path.join(root, name)
         imageName, ext = name.split(".")
@@ -16,7 +16,7 @@ for root, dirs, files in tqdm(os.walk(pathToAnnotations, topdown=False)):
         url = 'https://app.nanonets.com/api/v2/ObjectDetection/Model/' + model_id + '/UploadFile/'
         data = {'file' :open(imagePath, 'rb'),  'data' :('', '[{"filename":"' + imageName+".jpg" + '", "object": '+ jsonData+'}]'),   'modelId' :('', model_id)}       
         response = requests.post(url, auth=requests.auth.HTTPBasicAuth(api_key, ''), files=data)
-        if response.status_code != 200:
-            print(response.text)
+        if response.status_code > 250 or response.status_code<200:
+            print(response.text), response.status_code
 
 print("\n\n\nNEXT RUN: python ./code/train-model.py")
